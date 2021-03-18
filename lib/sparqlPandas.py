42fa7329b7c0edbc115a1f8175edde0b22c271fc
Douglas Fils <drfils@gmail.com>
2021-03-18 11:37:42 -0500

    library testing

---
+++ lib/sparqlPandas.py
@@ -0,0 +1,21 @@
+#@title
+def get_sparql_dataframe(service, query):
+    """
+    Helper function to convert SPARQL results into a Pandas data frame.
+    """
+    sparql = SPARQLWrapper(service)
+    sparql.setQuery(query)
+    sparql.setReturnFormat(JSON)
+    result = sparql.query()
+
+    processed_results = json.load(result.response)
+    cols = processed_results['head']['vars']
+
+    out = []
+    for row in processed_results['results']['bindings']:
+        item = []
+        for c in cols:
+            item.append(row.get(c, {}).get('value'))
+        out.append(item)
+
+    return pd.DataFrame(out, columns=cols)
